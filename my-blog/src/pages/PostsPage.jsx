b019890db09754abae88cae7ddd9cafb0ae9a62e
hrvojerados <hrvoje.rados8@gmail.com>
2025-09-20 17:53:48 +0200

    saving progress

--- my-blog/src/pages/PostsPage.jsx
+++ my-blog/src/pages/PostsPage.jsx
@@ -1,22 +1,68 @@
 import posts from "../posts.json";
 import tags from "../tags.json";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
+function formatDate(dateString) {
+  const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  const [year, month, day] = dateString.split("-").map(Number);
+  const monthName = months[month - 1];
+
+  return `${monthName} ${day}, ${year}`;
+}
+
+function formatTags(tags) {
+  let val = "";
+  for (const tag of tags) {
+    val  += ("#" + tag + "   ");
+  }
+  return val;
+}
+
+function toogleTag(tag, selected) {
+  if (selected.includes(tag)) {
+    return selected.filter((t) => (t != tag));
+  } else {
+    return [... selected, tag];
+  }
+}
+
+function hasIntersection(arr1, arr2) {
+  return arr1.some(item => arr2.includes(item));
+}
+
+console.log(hasIntersection([1, 2, 3], [3, 4, 5])); // true
+console.log(hasIntersection([1, 2], [3, 4]));       // false
 export default function PostsPage() {
+  const [selectedTags, setSelectedTags] = useState(tags);
+
   return (
     <div id="postsPage">
       <div id="postsContainer">
+        <img src="images/filter.svg" id="filterSvg"/> 
         <div className="tagsContainer">
           {tags.map((tag) => (
-            <div className="tag">
-              tag
-            </div>
+            <button 
+              className={selectedTags.includes(tag) ? "selectedTag" : "tag"}
+              onClick={() => setSelectedTags((prev) => toogleTag(tag, prev))}>
+              {tag}
+            </button>
           ))}
         </div>
-        {posts.map((post) => (
+        {posts.map((post) => hasIntersection(post.tags, selectedTags) && (
           <div className="Post" key={post.slug}>
             <div className="PostCard">
-              <div className="PostTitle">{post.title}</div>
+              <div className="PostData">
+                <div className="PostTitle">{post.title}</div>
+                <div className="DateAndTags">
+                  <p className="Date">{formatDate(post.date)}</p>
+                  <p className="Tags">{formatTags(post.tags)}</p>
+                </div>
+              </div>
               <Link to={`/posts/${post.slug}`} className="openPostLink">
                 <div className="open">
                   Open
