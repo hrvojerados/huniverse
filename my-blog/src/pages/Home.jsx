941df3e10707203ad516edf351f4d586bc7f6fed
hrvojerados <hrvoje.rados8@gmail.com>
2025-09-21 11:23:35 +0200

    Testing if secret environment file will end up on github

--- my-blog/src/pages/Home.jsx
+++ my-blog/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
-export default function Home() {
 
+export default function Home() {
+  // console.log(import.meta.env.VITE_ENV);
   return (
     <div className="home">
       <div className="HomeTitle">
--- my-blog/src/pages/Post.jsx
+++ my-blog/src/pages/Post.jsx
@@ -1,15 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import remarkMath from "remark-math";
-import remarkBreaks from "remark-breaks";
-import remarkFootnotes from "remark-footnotes";
-import rehypeKatex from "rehype-katex";
-import rehypeHighlight from "rehype-highlight";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeExternalLinks from "rehype-external-links";
 
 import posts from "../posts.json";
 import {formatDate} from "../utils.js";
@@ -35,16 +26,7 @@ export default function Post() {
       <p>{formatDate(post.date)}</p>
       <hr />
       <div className="pageContent">
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm, remarkMath, remarkBreaks, remarkFootnotes]}
-          rehypePlugins={[
-            rehypeKatex,
-            rehypeHighlight,
-            rehypeSlug,
-            [rehypeAutolinkHeadings, { behavior: "wrap" }],
-            [rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] }],
-          ]}
-        >
+        <ReactMarkdown>
           {content}
         </ReactMarkdown>
       </div>
