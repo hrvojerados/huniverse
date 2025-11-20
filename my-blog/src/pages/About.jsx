5c62afc03caa71e11a4ec97698aace4d399ec938
hrvojerados <hrvoje.rados8@gmail.com>
2025-11-20 10:32:30 +0100

    Cleaned up the posts used for testing

--- my-blog/src/pages/About.jsx
+++ my-blog/src/pages/About.jsx
@@ -18,13 +18,22 @@ export default function About() {
           on which I have worked on or on which I'm 
           working on. But I also want people 
           to get to know me through my own space on 
-          the Internet.
+          the Interneet.
       </div>
       <div className="pageContent">
           I plan to post a wide variety of posts here
           (hence the "universe" in the name) and I 
           hope you'll enjoy reading them.
       </div>
+      <h2>GitHub:</h2>
+      <div>
+          <a href="https://github.com/hrvojerados">GitHub_hrvojerados</a>
+      </div>
+      <h2>LinkedIn:</h2>
+      <div>
+          <a href="https://www.linkedin.com/in/hrvoje-rado%C5%A1-8797a224a/?originalSubdomain=hr">LinkedIn</a>
+      </div>
+
     </div>
     </>
   );
